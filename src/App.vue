9a03b19f16f6259ab4961cd53e6ec772947f0f48
Highfive universite <huniversite@chem.loc>
2025-11-19 18:16:21 +0100

    mmm

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script setup>
+import HeadSection from './components/HeadSection.vue';
+
+</script>
 
 <template>
-  <h1 class="text-3xl text-red-500">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <HeadSection />
 </template>
 
 <style scoped></style>
